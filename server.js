0dfdfba5613ea0848d6f3e5d0f591daaaa198840
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-01 18:26:57 -0500

    changing port number

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// website name is lychee-cobbler-55273
+// website name is https://git.heroku.com/lychee-cobbler-55273.git
 const express = require("express");
 // const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
