e1e53db8e8385002e9a1b48c9fe7b4b0be47d8fe
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-01 18:52:05 -0500

    troubleshooting

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: true
+  useUnifiedTopology: true
 });
 
 // routes
