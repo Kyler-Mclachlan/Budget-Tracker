bc607404988952b979645707c573ea60362abf54
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-02 14:56:50 -0500

    fixing cache

--- public/js/idb.js
+++ public/js/idb.js
@@ -28,9 +28,53 @@ function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_object_store_PWA_Challenge'], 'readwrite');
   
-    // access the object store for `new_pizza`
-    const pizzaObjectStore = transaction.objectStore('new_object_store_PWA_Challenge');
-  
-    // add record to your store with add method
-    pizzaObjectStore.add(record);
+    const budgetObjectStore = transaction.objectStore('new_object_store_PWA_Challenge');
+
+    // add record to your store with add method.
+    budgetObjectStore.add(record);
   }
+
+
+  function uploadBudget() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_object_store_PWA_Challenge'], 'readwrite');
+  
+    // access your pending object store
+    const budgetObjectStore = transaction.objectStore('new_object_store_PWA_Challenge');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+      // if there was data in indexedDb's store, let's send it to the api server
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(serverResponse => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+  
+            const transaction = db.transaction(['new_object_store_PWA_Challenge'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('new_object_store_PWA_Challenge');
+            // clear all items in your store
+            budgetObjectStore.clear();
+          })
+          .catch(err => {
+            // set reference to redirect back here
+            console.log(err);
+          });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener('online', uploadBudget);
+  
