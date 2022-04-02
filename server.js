90691495ba3e171d4e67ca08d725139dc74df4cd
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-01 19:05:07 -0500

    troubleshooting

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// website name is lychee-cobbler-55273
 const express = require("express");
-// const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -17,12 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: true
+  useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
