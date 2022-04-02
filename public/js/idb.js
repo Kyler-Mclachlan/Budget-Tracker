f27f4e355728b08684fcb23375d4ce8c030cc1b0
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-02 15:31:03 -0500

    fixing cache

--- public/js/idb.js
+++ public/js/idb.js
@@ -46,6 +46,7 @@ function saveRecord(record) {
     const getAll = budgetObjectStore.getAll();
     console.log('line 47!');
     getAll.onsuccess = function() {
+      console.log(getAll);
       // if there was data in indexedDb's store, let's send it to the api server
       if (getAll.result.length > 0) {
         fetch('/api/transaction/bulk', {
