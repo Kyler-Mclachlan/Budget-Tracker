5c7ab718df63491a51ab613785a2f1e1514f62b0
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-02 14:13:40 -0500

    removed info

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,15 +6,7 @@ const FILES_TO_CACHE = [
     './index.html',
     './manifest.json',
     './css/styles.css',
-    './js/index.js',
-    './icons/icon-72x72.png',
-    './icons/icons/icon-96x96.png',
-    './icons/icons/icon-128x128.png',
-    './icons/icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png'
+    './js/index.js'
   ];
 
   self.addEventListener('install', function(evt) {
